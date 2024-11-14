1aba3fef1bda5a90b1cec89d41cdf9197ffccd01
Rosexsy <137040538+Rosexsy@users.noreply.github.com>
2024-11-14 19:49:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // script.js
-function showMore() {
-    alert("Here is more information about the Loy Krathong Festival!");
+function showFestivalDetails() {
+    alert("เทศกาลลอยกระทงเป็นงานเฉลิมฉลองทางวัฒนธรรมที่มีความสำคัญในประเทศไทย...");
 }
+
